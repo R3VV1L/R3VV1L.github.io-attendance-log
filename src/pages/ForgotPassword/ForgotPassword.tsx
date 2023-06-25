3cac0ca6b8a87d1cacf380b457b76dde0e649272
v3r3tnxv <alexanderveretnov@gmail.com>
2023-06-25 20:46:41 +0300

    поправит верстку авторизации

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,7 +1,15 @@
 // @ts-ignore
 import React from "react";
 import "./ForgotPassword.css";
+import { SguSvg } from "../../assets/SguSvg.tsx";
 
 export const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  
+  return (
+    <div className="forgot-block">
+      <SguSvg />
+      <p className="forgot-block-head-text">Восстановление пароля</p>
+      <p className="forgot-block-info-text">Введите ваш e-mail и мы отправим на него инструкции по восстановлению пароля</p>
+    </div>
+  );
 };
