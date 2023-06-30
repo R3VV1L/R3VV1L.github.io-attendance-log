9552001f726623b2c9e54af487752e9376a9b121
v3r3tnxv <alexanderveretnov@gmail.com>
2023-06-30 14:56:57 +0300

    Изменена форма авторизации

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export const NotFound = () => {
   return (
     <div>
-      <NavLink to={"/"}>перейти к авторизации</NavLink>
+      <NavLink to={"/"}>Перейти к авторизации</NavLink>
       <h1>404</h1>
       <h2>404</h2>
       <h3>404</h3>
