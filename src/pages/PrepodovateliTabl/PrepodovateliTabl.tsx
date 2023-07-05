134259421699bf567a14634afadbf0f4ea4a8377
v3r3tnxv <alexanderveretnov@gmail.com>
2023-07-05 15:43:47 +0300

    Добавит не добавит

--- src/pages/PrepodovateliTabl/PrepodovateliTabl.tsx
+++ src/pages/PrepodovateliTabl/PrepodovateliTabl.tsx
@@ -1,11 +1,13 @@
 // @ts-ignore
 import React from "react";
 import { BurgerMenuAdmin } from "../../components/BurgerMenuAdmin/BurgerMenuAdmin.tsx";
+import Tabl from "../../components/Tabl/Tabl.tsx";
 
 export const PrepodovateliTabl = () => {
   return (
     <div>
       <BurgerMenuAdmin />
+      <Tabl />
     </div>
   );
 };
