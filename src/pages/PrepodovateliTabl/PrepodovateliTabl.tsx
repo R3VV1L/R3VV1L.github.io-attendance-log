c71b246f72707ce535d1180ba276e9db14101176
R3VV1L <r3vv1l@mail.ru>
2023-07-05 13:45:33 +0300

    master-frontend / добавит поиск

--- src/pages/PrepodovateliTabl/PrepodovateliTabl.tsx
+++ src/pages/PrepodovateliTabl/PrepodovateliTabl.tsx
@@ -1,11 +1,15 @@
 // @ts-ignore
 import React from "react";
 import { BurgerMenuAdmin } from "../../components/BurgerMenuAdmin/BurgerMenuAdmin.tsx";
+import { Search } from "../../components/search/Search.tsx";
+import { ModalGroup } from "../../components/ModalGroup/ModalGroup.tsx";
 
 export const PrepodovateliTabl = () => {
   return (
     <div>
+      <Search />
       <BurgerMenuAdmin />
+      <ModalGroup />
     </div>
   );
 };
