2fe140f20f1371320071a30e9f12daf67b651b77
v3r3tnxv <alexanderveretnov@gmail.com>
2023-07-06 12:01:24 +0300

    Исправлена ошибка со сборкой

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,28 +1,29 @@
 import "./Header.css";
-import { NavLink } from "react-router-dom";
-
-// export const Header = () => {
-//   return (
-//     <div className="box">
-//       <div className="rectangle-wrapper">
-//         <div className="rectangle" />
-//       </div>
-//     </div>
-//   );
-// };
+// import { NavLink } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <NavLink to="/attendance/students" className="link">Студенты</NavLink>
-          <NavLink to="/attendance/teachers" className="link">Преподаватели</NavLink>
-          <NavLink to="/attendance/groups" className="link">Группы</NavLink>
-          <NavLink to="/attendance/subgroups" className="link">Подгруппы</NavLink>
-          <NavLink to="/auth" className="link">Выйти</NavLink>
-        </ul>
-      </nav>
-    </header>
+    <div className="box">
+      <div className="rectangle-wrapper">
+        <div className="rectangle" />
+      </div>
+    </div>
   );
 };
+
+// Экспериментальный навбар
+// export const Header = () => {
+//   return (
+//     <header>
+//       <nav>
+//         <ul>
+//           <NavLink to="/attendance/students" className="link">Студенты</NavLink>
+//           <NavLink to="/attendance/teachers" className="link">Преподаватели</NavLink>
+//           <NavLink to="/attendance/groups" className="link">Группы</NavLink>
+//           <NavLink to="/attendance/subgroups" className="link">Подгруппы</NavLink>
+//           <NavLink to="/auth" className="link">Выйти</NavLink>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// };
