6bfc574ee0d33c64043e401bcd87fb52a16ea6b5
v3r3tnxv <alexanderveretnov@gmail.com>
2023-07-12 13:16:46 +0300

    Добавит обработчики событий для полей ввода

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -24,6 +24,22 @@ export const Search = ({ onSearch }: SearchProps) => {
         handleSearch();
     };
 
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setQuery(event.target.value);
+    };
+
+    const handleInputFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+        if (!event.target.value) {
+            event.target.placeholder = '';
+        }
+    };
+
+    const handleInputBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+        if (!event.target.value) {
+            event.target.placeholder = 'Поиск...';
+        }
+    };
+
     return (
         <form className="search-box" onSubmit={handleSubmit}>
             <input
@@ -32,7 +48,12 @@ export const Search = ({ onSearch }: SearchProps) => {
                 placeholder="Поиск..."
                 required
                 value={query}
-                onChange={(event) => setQuery(event.target.value)}
+                onChange={(event) => {
+                    setQuery(event.target.value);
+                    handleInputChange(event);
+                }}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
                 onKeyPress={handleKeyPress}
             />
             <button type="submit" className="search-bottom">
