d3d2d548373f44753b8026dbc46d6989409b8ecd
R3VV1L <r3vv1l@mail.ru>
2023-08-23 12:50:04 +0300

    master-frontend / добавит функциональность кнопкам таблиц

--- src/components/tables/table/Table.tsx
+++ src/components/tables/table/Table.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@reduxjs/toolkit/dist/query/core/apiState';
 import { setTableLength } from '../../../API/action.tsx';
 import { Search } from '../../search/Search.tsx';
+import { ButtonTable } from '../button-table/ButtonTable.tsx';
 
 interface TableProps {
     data: any;
@@ -14,7 +15,19 @@ export const Table = ({ data, user }: TableProps) => {
     // @ts-ignore
     const [tableData, setTableData] = useState(data);
     console.log(user);
+
+    /////////////////////////// функции для отображения записей в кнопочках
     const [filteredData, setFilteredData] = useState(data);
+    // @ts-ignore
+    const [currentPage, setCurrentPage] = useState(1);
+    const recordsPerPage = 15;
+
+    const getPaginatedData = () => {
+        const startIndex = (currentPage - 1) * recordsPerPage;
+        const endIndex = startIndex + recordsPerPage;
+        return filteredData.slice(startIndex, endIndex);
+    };
+    ///////////////////////////
 
     useSelector((state: RootState<any, any, any>) => state.teachers);
     const tableLength = filteredData.length;
@@ -27,8 +40,8 @@ export const Table = ({ data, user }: TableProps) => {
             a.name.localeCompare(b.name)
         );
         setFilteredData(sorted);
-        dispatch(setTableLength(filteredData.length));
-    }, [dispatch, tableLength, filteredData]);
+        dispatch(setTableLength(sorted.length));
+    }, [dispatch, filteredData]);
 
     const handleSearch = (query: string) => {
         const filtered = tableData.filter((item: any) =>
@@ -87,7 +100,7 @@ export const Table = ({ data, user }: TableProps) => {
                 </thead>
                 {user === 'group' ? (
                     <tbody>
-                        {filteredData.map((group: any, index: number) => (
+                        {getPaginatedData().map((group: any, index: number) => (
                             <tr key={index}>
                                 <td className="table-td">{index + 1}</td>
                                 <td className="table-td">{group.name}</td>
@@ -101,51 +114,80 @@ export const Table = ({ data, user }: TableProps) => {
                 ) : null}
                 {user === 'subgroup' ? (
                     <tbody>
-                        {filteredData.map((subgroup: any, index: number) => (
-                            <tr key={index}>
-                                <td className="table-td">{index + 1}</td>
-                                <td className="table-td">{subgroup.name}</td>
-                                <td className="table-td">
-                                    {subgroup.comments}
-                                </td>
-                            </tr>
-                        ))}
+                        {getPaginatedData().map(
+                            (subgroup: any, index: number) => (
+                                <tr key={index}>
+                                    <td className="table-td">{index + 1}</td>
+                                    <td className="table-td">
+                                        {subgroup.name}
+                                    </td>
+                                    <td className="table-td">
+                                        {subgroup.comments}
+                                    </td>
+                                </tr>
+                            )
+                        )}
                     </tbody>
                 ) : null}
                 {user === 'teacher' ? (
                     <tbody>
-                        {filteredData.map((teacher: any, index: number) => (
-                            <tr key={index}>
-                                <td className="table-td">{index + 1}</td>
-                                <td className="table-td">{teacher.name}</td>
-                                <td className="table-td">{teacher.position}</td>
-                                <td className="table-td">
-                                    {teacher.department}
-                                </td>
-                                <td className="table-td">{teacher.email}</td>
-                                <td className="table-td">{teacher.login}</td>
-                                <td className="table-td">{teacher.password}</td>
-                            </tr>
-                        ))}
+                        {getPaginatedData().map(
+                            (teacher: any, index: number) => (
+                                <tr key={index}>
+                                    <td className="table-td">{index + 1}</td>
+                                    <td className="table-td">{teacher.name}</td>
+                                    <td className="table-td">
+                                        {teacher.position}
+                                    </td>
+                                    <td className="table-td">
+                                        {teacher.department}
+                                    </td>
+                                    <td className="table-td">
+                                        {teacher.email}
+                                    </td>
+                                    <td className="table-td">
+                                        {teacher.login}
+                                    </td>
+                                    <td className="table-td">
+                                        {teacher.password}
+                                    </td>
+                                </tr>
+                            )
+                        )}
                     </tbody>
                 ) : null}
                 {user === 'student' ? (
                     <tbody>
-                        {filteredData.map((student: any, index: number) => (
-                            <tr key={index}>
-                                <td className="table-td">{index + 1}</td>
-                                <td className="table-td">{student.name}</td>
-                                <td className="table-td">
-                                    {student.department}
-                                </td>
-                                <td className="table-td">{student.email}</td>
-                                <td className="table-td">{student.login}</td>
-                                <td className="table-td">{student.password}</td>
-                            </tr>
-                        ))}
+                        {getPaginatedData().map(
+                            (student: any, index: number) => (
+                                <tr key={index}>
+                                    <td className="table-td">{index + 1}</td>
+                                    <td className="table-td">{student.name}</td>
+                                    <td className="table-td">
+                                        {student.department}
+                                    </td>
+                                    <td className="table-td">
+                                        {student.email}
+                                    </td>
+                                    <td className="table-td">
+                                        {student.login}
+                                    </td>
+                                    <td className="table-td">
+                                        {student.password}
+                                    </td>
+                                </tr>
+                            )
+                        )}
                     </tbody>
                 ) : null}
             </table>
+            <ButtonTable
+                currentPage={currentPage}
+                recordsPerPage={recordsPerPage}
+                filteredData={filteredData}
+                setCurrentPage={setCurrentPage}
+                getPaginatedData={getPaginatedData}
+            />
         </div>
     );
 };
